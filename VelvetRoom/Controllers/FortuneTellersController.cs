d83f971367b2fb75169515e5445d849636fc2687
Tamara <tamara.volic@hotmail.com>
2022-06-03 08:14:11 +0200

    Projektdateien hinzufügen.

---
+++ VelvetRoom/Controllers/FortuneTellersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace VelvetRoom.Controllers
+{
+    public class FortuneTellersController : Controller
+    {
+        public IActionResult FortuneTelling()
+        {
+            return View();
+        }
+    }
+}
