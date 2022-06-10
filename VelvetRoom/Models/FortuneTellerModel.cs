d584b1f82fd10b3ee30dd7db86f9741f929a9fc4
Tamara <tamara.volic@hotmail.com>
2022-06-10 11:27:58 +0200

    Teller zum essen

--- VelvetRoom/Models/FortuneTellerModel.cs
+++ VelvetRoom/Models/FortuneTellerModel.cs
@@ -5,6 +5,8 @@ namespace VelvetRoom.Models
 {
     public class FortuneTellerModel
     {
-        FortuneTeller fortuneTeller = new FortuneTeller("igor", "Igor takes pride in his professional tellings and straightforward approach. He will always tell the customer what the cards say with no regards to what the tellings said.", "~/img/FortuneTellers/Igor.png");
+        FortuneTeller fortuneTellerIgor = new FortuneTeller("igor", "Igor takes pride in his professional tellings and straightforward approach. He will always tell the customer what the cards say with no regards to what the tellings said.", "~/img/FortuneTellers/Igor.png");
+        FortuneTeller fortuneTellerUrsula = new FortuneTeller("Ursula", "Ursula is a sinister woman that loves to tell the Costumers the worst possible interpretation of their Fate. She gloats about bad readings and downplays good ones.", "~/img/FortuneTellers /Ursula.png");
+        FortuneTeller fortuneTellerScheherazade = new FortuneTeller("Scheherazade", "abcdefghijklmnopqrstuvwxyz yuhe.", "~/img/FortuneTellers/Scheherazade.jpg");
     }
 }
