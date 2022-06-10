cf2deea407d5cd67b6c35435fc74b03d886f5dcc
Tamara <tamara.volic@hotmail.com>
2022-06-10 12:19:27 +0200

    Push it

---
+++ VelvetRoom/Models/TarotCardModel.cs
@@ -0,0 +1,9 @@
+﻿using VelvetRoom.Views.Shared;
+
+namespace VelvetRoom.Models
+{
+    public class TarotCardModel
+    {
+        TarotCard tartotCardTheFool = new TarotCard("NR.0 The Fool", "Depicting a youg man and his faithfull Dog, the Fool symbolises the beginning of a jorney and the unlimited possibilities therof.Assigned the number 0, this place with the other cards is hard to tell but he is ever present nonethless.", "~/img/TarotCards/TheFool.png");
+    }
+}
---
+++ VelvetRoom/Views/Shared/TarotCard.cs
@@ -0,0 +1,11 @@
+﻿namespace VelvetRoom.Views.Shared
+{
+    public class TarotCard
+    {
+        public TarotCard(string title, string desicription, string image) { this.title = title; this.desicription = desicription; this.image = image; }
+        string title { set; get; }
+        string desicription { set; get; }
+        string image { set; get; }
+
+    }
+}
